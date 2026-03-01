51188671e2890d5c9ab5d81c9edeb4fd1c0eae11
LiWenyue <1074168216@qq.com>
2026-03-01 08:52:00 +0800

    saving changes

--- lec01/homework1.py
+++ lec01/homework1.py
@@ -12,6 +12,5 @@ string1 should be changed from "change this" to "test"
 string2 should be changed from "change this" to "hello"
 '''
 
-string1 = "change this"
-
-string2 = "change this"
+string1 = "test"  # 已修改为指定值
+string2 = "hello" # 已修改为指定值
